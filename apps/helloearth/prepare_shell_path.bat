ba691c4d383ef07193192bdf50bb1ba44a7cbdcf
eyob.boye <eyob.boye@gmail.com>
2017-06-12 22:50:21 -0400

    Document purpose of prepare_shell_path.bat

--- apps/helloearth/prepare_shell_path.bat
+++ apps/helloearth/prepare_shell_path.bat
@@ -1,2 +1,11 @@
+@ECHO OFF
+REM ***************************************************************************
+REM I like using windows cmd instead of msys2 shell. For some reason I have
+REM difficulty running scons on msys2. Therefore I run this batch file
+REM to allow me to run the esp-idf make on the windows cmd shell. Once this
+REM batch file runs, I can issue "make flash" to run the esp-idf makefile.
+REM Your install is probably different than mine so you need to change 
+REM these paths accordingly.
+REM ***************************************************************************
 PATH="C:\Tools\msys32\var\opt\xtensa-esp32-elf-1.22.0-61-gab8375a-5.2.0\bin";%PATH%
 PATH="C:\Tools\msys32\usr\bin";%PATH%
